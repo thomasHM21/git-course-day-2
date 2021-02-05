0b86586dc6a526c8be3a59480f1aeab6ee910036
thomasHM21 <thomas.forsberg@hm.com>
2021-02-05 09:42:40 +0100

    Minor changes to Main

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@ public class Main {
     public static void main(String[] args) {
         int num = 100;
         System.out.println(num);
-        System.out.println(increment(num));
+
+        int num2 = increment(num);
+        System.out.println(num2);
+        System.out.println(increment(num2));
     }
 
     public static int increment(int number){
