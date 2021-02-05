4803696190971bd13fe3f706f13db0c5cdf7035a
thomasHM21 <thomas.forsberg@hm.com>
2021-02-05 11:17:52 +0100

    Minor changes to printing

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        int num = 100;
+        int num = 10;
         System.out.println(num);
 
         num = increment(num);
-        System.out.printf("Increment by 1\n%d",num);
+        System.out.printf("Increment number by 1\n%d",num);
         num = decrement(num);
-        System.out.printf("\nDecrement by 1\n%d",num);
+        System.out.printf("\nDecrement number by 1\n%d",num);
 
     }
 
